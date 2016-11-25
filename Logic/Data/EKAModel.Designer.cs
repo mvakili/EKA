9bcda3b10c5fc8caa850b0387215b10fa5c6fe16
mvakili <m.vakili95@gmail.com>
2016-11-25 21:21:44 +0330

    #1 Logic Layer Initialized

--- Logic/Data/EKAModel.Designer.cs
+++ Logic/Data/EKAModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\MV\Dropbox\EKA\EKA\Logic\Data\EKAModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\EKA\Logic\Data\EKAModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
