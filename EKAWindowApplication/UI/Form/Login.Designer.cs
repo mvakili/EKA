d64b80dbaaee659ea68be3b06b0eba28bbf71072
Bita <Bita@MASSOUD>
2016-11-28 22:08:52 +0330

    test

--- EKAWindowApplication/UI/Form/Login.Designer.cs
+++ EKAWindowApplication/UI/Form/Login.Designer.cs
@@ -48,44 +48,49 @@
             // 
             // lblUserName
             // 
-            this.lblUserName.Location = new System.Drawing.Point(213, 179);
+            this.lblUserName.Location = new System.Drawing.Point(160, 145);
+            this.lblUserName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lblUserName.Name = "lblUserName";
             this.lblUserName.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.lblUserName.Size = new System.Drawing.Size(67, 22);
+            this.lblUserName.Size = new System.Drawing.Size(54, 18);
             this.lblUserName.TabIndex = 0;
             this.lblUserName.Text = "نام کاربری:";
             this.lblUserName.TextAlignment = System.Drawing.ContentAlignment.TopRight;
             // 
             // radLabel1
             // 
-            this.radLabel1.Location = new System.Drawing.Point(213, 223);
+            this.radLabel1.Location = new System.Drawing.Point(160, 181);
+            this.radLabel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.radLabel1.Name = "radLabel1";
             this.radLabel1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.radLabel1.Size = new System.Drawing.Size(59, 22);
+            this.radLabel1.Size = new System.Drawing.Size(48, 18);
             this.radLabel1.TabIndex = 1;
             this.radLabel1.Text = "رمز عبور:";
             this.radLabel1.TextAlignment = System.Drawing.ContentAlignment.TopRight;
             // 
             // txtUserName
             // 
-            this.txtUserName.Location = new System.Drawing.Point(70, 177);
+            this.txtUserName.Location = new System.Drawing.Point(52, 144);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(125, 24);
+            this.txtUserName.Size = new System.Drawing.Size(94, 20);
             this.txtUserName.TabIndex = 2;
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(70, 223);
+            this.txtPassword.Location = new System.Drawing.Point(52, 181);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(125, 24);
+            this.txtPassword.Size = new System.Drawing.Size(94, 20);
             this.txtPassword.TabIndex = 3;
             // 
             // btnLogin
             // 
-            this.btnLogin.Location = new System.Drawing.Point(70, 278);
+            this.btnLogin.Location = new System.Drawing.Point(52, 226);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(92, 30);
+            this.btnLogin.Size = new System.Drawing.Size(69, 24);
             this.btnLogin.TabIndex = 4;
             this.btnLogin.Text = "ورود";
             this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
@@ -95,9 +100,10 @@
             this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(110, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(52, 11);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(97, 95);
+            this.pictureBox1.Size = new System.Drawing.Size(73, 77);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
@@ -105,20 +111,21 @@
             // lblResult
             // 
             this.lblResult.AutoSize = false;
-            this.lblResult.Location = new System.Drawing.Point(12, 141);
+            this.lblResult.Location = new System.Drawing.Point(9, 115);
+            this.lblResult.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lblResult.Name = "lblResult";
             this.lblResult.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.lblResult.Size = new System.Drawing.Size(308, 30);
+            this.lblResult.Size = new System.Drawing.Size(231, 24);
             this.lblResult.TabIndex = 6;
             this.lblResult.TextAlignment = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // Login
             // 
             this.AcceptButton = this.btnLogin;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(342, 340);
+            this.ClientSize = new System.Drawing.Size(264, 279);
             this.Controls.Add(this.lblResult);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.btnLogin);
@@ -126,17 +133,16 @@
             this.Controls.Add(this.txtUserName);
             this.Controls.Add(this.radLabel1);
             this.Controls.Add(this.lblUserName);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(350, 370);
+            this.MaximumSize = new System.Drawing.Size(272, 309);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(350, 370);
+            this.MinimumSize = new System.Drawing.Size(272, 309);
             this.Name = "Login";
             // 
             // 
             // 
             this.RootElement.ApplyShapeToControl = true;
-            this.RootElement.MaxSize = new System.Drawing.Size(350, 370);
+            this.RootElement.MaxSize = new System.Drawing.Size(272, 309);
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ورود";
